6033ff35c1c22eb755cf79c02864fa6a3cdcfed5
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-04-05 17:25:36 +0530

    Initial Home Screen made

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:medicine_reminder_app/constants/colors.dart';
 import 'package:medicine_reminder_app/screens/home_screen.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,14 +16,101 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Medicine Reminder',
-      theme: ThemeData.dark(useMaterial3: true).copyWith(
-        scaffoldBackgroundColor: bgColor,
-        appBarTheme: const AppBarTheme(color: appBarColor),
-      ),
-      home: const HomeScreen(),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Medicine Reminder',
+          theme: ThemeData.dark(useMaterial3: true).copyWith(
+            primaryColor: kPrimaryColor,
+            scaffoldBackgroundColor: kScaffoldColor,
+            appBarTheme: AppBarTheme(
+              toolbarHeight: 7.h,
+              backgroundColor: kScaffoldColor,
+              elevation: 0,
+              iconTheme: IconThemeData(
+                color: kSecondaryColor,
+                size: 20.sp,
+              ),
+              titleTextStyle: GoogleFonts.mulish(
+                color: kTextColor,
+                fontWeight: FontWeight.w800,
+                fontStyle: FontStyle.normal,
+                fontSize: 16.sp,
+              ),
+            ),
+
+            textTheme: TextTheme(
+              displaySmall: TextStyle(
+                fontSize: 28.sp,
+                color: kSecondaryColor,
+                fontWeight: FontWeight.w500,
+              ),
+              headlineMedium: GoogleFonts.poppins(
+                fontSize: 24.sp,
+                fontWeight: FontWeight.w600,
+                color: kTextColor,
+              ),
+              headlineSmall: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w900,
+                color: kTextColor,
+              ),
+              titleLarge: GoogleFonts.poppins(
+                fontSize: 13.sp,
+                color: kTextColor,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.0,
+              ),
+              titleMedium:
+                  GoogleFonts.poppins(fontSize: 15.sp, color: kPrimaryColor),
+              titleSmall:
+                  GoogleFonts.poppins(fontSize: 12.sp, color: kTextLightColor),
+              bodySmall: GoogleFonts.poppins(
+                fontSize: 9.sp,
+                fontWeight: FontWeight.w400,
+                color: kTextLightColor,
+              ),
+              labelMedium: TextStyle(
+                fontSize: 10.sp,
+                fontWeight: FontWeight.w500,
+                color: kTextColor,
+              ),
+            ),
+            inputDecorationTheme: const InputDecorationTheme(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: kTextLightColor,
+                  width: 0.7,
+                ),
+              ),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: kTextLightColor),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: kPrimaryColor),
+              ),
+            ),
+
+            //** customize the timePicker theme ->
+            timePickerTheme: TimePickerThemeData(
+              backgroundColor: kScaffoldColor,
+              hourMinuteColor: kTextColor,
+              hourMinuteTextColor: kScaffoldColor,
+              dayPeriodColor: kTextColor,
+              dayPeriodTextColor: kScaffoldColor,
+              dialBackgroundColor: kTextColor,
+              dialHandColor: kPrimaryColor,
+              dialTextColor: kScaffoldColor,
+              entryModeIconColor: kOtherColor,
+              dayPeriodTextStyle: GoogleFonts.aBeeZee(
+                fontSize: 8.sp,
+              ),
+            ),
+          ),
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:medicine_reminder_app/constants/colors.dart';
+import 'package:sizer/sizer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,9 +8,96 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pill Reminder'),
-        centerTitle: true,
+      appBar: AppBar(),
+
+      //?? body ->
+      body: Padding(
+        padding: EdgeInsets.all(2.h),
+        child: Column(
+          children: [
+            const TopContainer(),
+            SizedBox(height: 2.h),
+            const Flexible(
+              child: BottomContainer(),
+            ),
+          ],
+        ),
+      ),
+
+      //?? floating action button ->
+      floatingActionButton: InkResponse(
+        onTap: () {},
+        child: SizedBox(
+          width: 18.w,
+          height: 9.h,
+          child: Card(
+            color: kPrimaryColor,
+            shape: BeveledRectangleBorder(
+              borderRadius: BorderRadius.circular(3.h),
+            ),
+            child: Icon(
+              Icons.add_outlined,
+              color: kScaffoldColor,
+              size: 50.sp,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//?? top container ->
+class TopContainer extends StatelessWidget {
+  const TopContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.only(bottom: 1.h),
+          child: Text(
+            'Worry less. \nLive healthier.',
+            textAlign: TextAlign.start,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          padding: EdgeInsets.only(bottom: 1.h),
+          child: Text(
+            'Welcome to Daily Dose.',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        SizedBox(height: 2.h),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(bottom: 1.h),
+          child: Text(
+            '0',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+//?? bottom container ->
+class BottomContainer extends StatelessWidget {
+  const BottomContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'No Medicine',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.displaySmall,
       ),
     );
   }
